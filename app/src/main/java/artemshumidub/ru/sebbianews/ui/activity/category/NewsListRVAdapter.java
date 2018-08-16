4bc3b9d52363c120627330352a3f0ea92fe4bf4c
artem.shumidub <artem.shumidub@gmail.com>
2018-08-16 14:50:45 +0300

    edit NewsListRVAdapter

--- app/src/main/java/artemshumidub/ru/sebbianews/ui/activity/category/NewsListRVAdapter.java
+++ app/src/main/java/artemshumidub/ru/sebbianews/ui/activity/category/NewsListRVAdapter.java
@@ -6,22 +6,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
 import artemshumidub.ru.sebbianews.R;
-import artemshumidub.ru.sebbianews.data.entity.Category;
+import artemshumidub.ru.sebbianews.data.entity.News;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class NewsListRVAdapter extends RecyclerView.Adapter<NewsListRVAdapter.Holder> {
 
-    //todo change type
-    private final List<Category> list;
+    private final List<News> list;
     private final Context context;
+    private OnItemListener onItemlistener;
 
-    NewsListRVAdapter(Context context, List<Category> list) {
+    NewsListRVAdapter(Context context, List<News> list) {
         this.list = list;
         this.context = context;
     }
@@ -34,8 +35,10 @@ public class NewsListRVAdapter extends RecyclerView.Adapter<NewsListRVAdapter.Ho
 
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
-        holder.itemTextView.setText(list.get(position).getName());
-        holder.itemTextView.setTag(list.get(position).getId());
+        holder.tvHead.setText(list.get(position).getTitle());
+        holder.tvShortDesc.setTag(list.get(position).getShortDescription());
+        int idNews = list.get(position).getId();
+        holder.llNewsItem.setOnClickListener((v)->onItemlistener.onItemClick(idNews));
     }
 
     @Override
@@ -44,12 +47,28 @@ public class NewsListRVAdapter extends RecyclerView.Adapter<NewsListRVAdapter.Ho
     }
 
     class Holder extends RecyclerView.ViewHolder {
-        @BindView(R.id.tv)
-        TextView itemTextView;
+
+        @BindView(R.id.ll_news_item)
+        LinearLayout llNewsItem;
+
+        @BindView(R.id.tv_head)
+        TextView tvHead;
+
+        @BindView(R.id.tv_short_desc)
+        TextView tvShortDesc;
 
         Holder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
     }
+
+    public void setOnItemlistener(OnItemListener onItemlistener) {
+        this.onItemlistener = onItemlistener;
+    }
+
+    interface OnItemListener{
+        void onItemClick(int id);
+    }
+
 }
