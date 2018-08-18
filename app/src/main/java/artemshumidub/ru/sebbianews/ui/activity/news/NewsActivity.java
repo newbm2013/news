51772249981af5af64ec153dab5219d30cfeeae6
ShumidubCoder <shumidubcoder@gmail.com>
2018-08-18 15:16:43 +0300

    news activity -> setup toolbar for showing title as one line when toolbar collapsed, and multiple line when toolbar expanded

--- app/src/main/java/artemshumidub/ru/sebbianews/ui/activity/news/NewsActivity.java
+++ app/src/main/java/artemshumidub/ru/sebbianews/ui/activity/news/NewsActivity.java
@@ -1,6 +1,5 @@
 package artemshumidub.ru.sebbianews.ui.activity.news;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -46,6 +45,9 @@ public class NewsActivity extends BaseActivity implements  INewsContract.IView{
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    @BindView(R.id.tv_toolbar_fulltext_title)
+    TextView tvExpandedToolbarTitle;
+
     @BindView(R.id.app_bar_layout)
     AppBarLayout appBarLayout;
 
@@ -55,55 +57,25 @@ public class NewsActivity extends BaseActivity implements  INewsContract.IView{
     private NewsPresenter presenter;
 
     public static final String ID_NEWS_KEY = "idNews";
-    long idNews;
+    private long idNews = 0;
+    private String news_title = "";
+    private String news_title_collapsed = "Новость";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         ButterKnife.bind(this);
-
-        idNews = getIntent().getExtras().getLong(ID_NEWS_KEY);
-
+        if (getIntent().getExtras()!=null){
+            idNews = getIntent().getExtras().getLong(ID_NEWS_KEY, 0);
+        }
         presenter= new NewsPresenter(this);
-        presenter.getNews(idNews);
-
         setSupportActionBar(toolbar);
         setAppBarLayoutExpandable(false);
-        collapsingToolbarLayout.setTitle("Новость");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home) this.finish();
-        return true;
-    }
-
-    public void setAppBarLayoutExpandable(boolean isExpandable) {
-        if (!isExpandable) {
-            appBarLayout.setExpanded(false, false);
-            appBarLayout.setActivated(false);
-            CoordinatorLayout.LayoutParams lp =
-                    (CoordinatorLayout.LayoutParams) appBarLayout.getLayoutParams();
-            lp.height = (int) getResources().getDimension(R.dimen.app_bar_collapsed_weight);
-        } else {
-            appBarLayout.setExpanded(true, false);
-            appBarLayout.setActivated(true);
-            CoordinatorLayout.LayoutParams lp =
-                    (CoordinatorLayout.LayoutParams) appBarLayout.getLayoutParams();
-            lp.height = (int) getResources().getDimension(R.dimen.app_bar_expanded_weight);
-        }
-    }
-
-    @Override
-    public void setNews(FullNews news) {
-        setAppBarLayoutExpandable(true);
-        contentView.setVisibility(View.VISIBLE);
-        tvFullNews.setText(Html.fromHtml(news.getFullDescription()));
-        tvFullNews.setMovementMethod(LinkMovementMethod.getInstance());
-        tvDate.setText(news.getDate());
-        collapsingToolbarLayout.setTitle(news.getTitle());
+        collapsingToolbarLayout.setTitle(news_title_collapsed);
+        if (getSupportActionBar()!=null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        appBarLayout.addOnOffsetChangedListener(getOnOffsetChangedListener());
+        presenter.getNews(idNews);
     }
 
     @Override
@@ -142,5 +114,62 @@ public class NewsActivity extends BaseActivity implements  INewsContract.IView{
     @Override
     public void stopProgress() {
         progressLayout.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home) this.finish();
+        return true;
+    }
+
+    @Override
+    public void setNews(FullNews news) {
+        news_title = news.getTitle();
+        tvExpandedToolbarTitle.setText(news_title);
+        setAppBarLayoutExpandable(true);
+        contentView.setVisibility(View.VISIBLE);
+        tvFullNews.setText(Html.fromHtml(news.getFullDescription()));
+        tvFullNews.setMovementMethod(LinkMovementMethod.getInstance());
+        tvDate.setText(news.getDate());
+    }
+
+    private void setAppBarLayoutExpandable(boolean isExpandable) {
+        if (!isExpandable) {
+            appBarLayout.setExpanded(false, false);
+            appBarLayout.setActivated(false);
+            CoordinatorLayout.LayoutParams lp =
+                    (CoordinatorLayout.LayoutParams) appBarLayout.getLayoutParams();
+            lp.height = (int) getResources().getDimension(R.dimen.app_bar_collapsed_weight);
+        } else {
+            appBarLayout.setExpanded(true, false);
+            appBarLayout.setActivated(true);
+            CoordinatorLayout.LayoutParams lp =
+                    (CoordinatorLayout.LayoutParams) appBarLayout.getLayoutParams();
+            lp.height = (int) getResources().getDimension(R.dimen.app_bar_expanded_weight);
+            lp.height = CoordinatorLayout.LayoutParams.WRAP_CONTENT;
+        }
+    }
+
+    private AppBarLayout.OnOffsetChangedListener getOnOffsetChangedListener(){
+        return new AppBarLayout.OnOffsetChangedListener() {
+             boolean isShow = false;
+             int scrollRange = -1;
+
+             @Override
+             public void onOffsetChanged(AppBarLayout appBarLayout1, int verticalOffset) {
+                 if (scrollRange == -1) {
+                     scrollRange = appBarLayout1.getTotalScrollRange();
+                 }
+                 if (scrollRange + verticalOffset == 0) {
+                     collapsingToolbarLayout.setTitle(" ");
+                     tvExpandedToolbarTitle.setVisibility(View.VISIBLE);
+                     isShow = true;
+                 } else if (isShow) {
+                     collapsingToolbarLayout.setTitle(news_title);
+                     tvExpandedToolbarTitle.setVisibility(View.INVISIBLE);
+                     isShow = false;
+                 }
+             }
+         };
     }
 }
